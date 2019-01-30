572fd823e43e591c816cdbab898b9ad35dfac1a2
Fyroman <djamic.dominik@gmail.com>
2019-01-30 16:48:18 +0100

    copied everything

---
+++ vuetify-snackbar.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import vuetifySnackbar from 'vuetifySnackbar.vue'
+
+const vSnackbar = {
+    install(Vue, options) {
+        const constParams = options;
+        Vue.prototype.$vSnackbar = (params) => {
+            const Snackbar = Vue.extend(vuetifySnackbar);
+            let bar = new Snackbar();
+
+            //params.type can be either 'warning', 'error', 'info' or 'success'
+            //it applies predefined colour and icon for snackbar
+
+            //rest of params are exactly the same as in vuetify snackbar docs
+            //if both params.color and params.type are set, colour takes precedent
+
+            bar.params = {...params, ...constParams} || {};
+
+            bar.$mount();
+            let appWrap = document.getElementById('app');
+            appWrap.appendChild(bar.$el);
+        }
+    }
+}
+
+export default vSnackbar;
---
+++ vuetifySnackbar.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-snackbar
+            v-model="snackbar"
+            :absolute="params.absolute"
+            :auto-height="params.autoHeight"
+            :bottom="params.bottom"
+            :color="params.color || typeColor()"
+            :left="params.left"
+            :multi-line="params.multiLine"
+            :right="params.right"
+            :timeout="params.timeout"
+            :top="params.top"
+            :value="params.value"
+            :vertical="params.vertical"
+            class="text-xs-left"
+    >
+        <v-icon left>{{ typeIcon() }}</v-icon>
+        {{ params.text }}
+        <v-spacer/>
+    </v-snackbar>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            snackbar: false,
+            params: {},
+        }),
+
+        methods: {
+            typeColor() {
+                if (!this.params.type)
+                    return null;
+                switch (this.params.type) {
+                    case 'error':
+                        return 'error';
+                    case 'success':
+                        return 'success';
+                    case 'warning':
+                        return 'warning';
+                    case 'info':
+                        return 'info';
+                    default:
+                        return null;
+                }
+            },
+            typeIcon() {
+                if (!this.params.type)
+                    return null;
+                switch (this.params.type) {
+                    case 'error':
+                        return 'report_problem';
+                    case 'success':
+                        return 'check_circle';
+                    case 'warning':
+                        return 'priority_high';
+                    case 'info':
+                        return 'info';
+                    default:
+                        return null;
+                }
+            },
+        },
+
+        mounted() {
+            this.$nextTick(() => {
+                this.snackbar = true;
+            })
+        }
+    }
+</script>
